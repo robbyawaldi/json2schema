0814dba305c2e101d37a79f51bcad4481a87c90c
Robby Awaldi <47056508+robbyawaldi@users.noreply.github.com>
2023-12-11 12:47:42 +0700

    support yaml output

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -4,15 +4,25 @@ import path from "path";
 import { jsonToOpenApiSchema } from "../jsonToSchema";
 import prettier from "prettier";
 import config from "../prettier";
+import YAML from "json-to-pretty-yaml";
 
-program.argument("<string>", "filename").action(async (str) => {
-  try {
-    const file = fs.readFileSync(path.resolve("./", str), "utf-8");
-    const obj = JSON.parse(file);
-    const schema = jsonToOpenApiSchema(obj);
-    const formattedCode = await prettier.format(JSON.stringify(schema), config);
-    console.log(formattedCode);
-  } catch (err) {}
-});
+program
+  .argument("<string>", "filename")
+  .option("-f, --format", "json or ymal")
+  .action(async (str, options) => {
+    try {
+      const file = fs.readFileSync(path.resolve("./", str), "utf-8");
+      const obj = JSON.parse(file);
+      const schema = jsonToOpenApiSchema(obj);
+      const jsonFormat = await prettier.format(JSON.stringify(schema), config);
+      const ymalFormat = YAML.stringify(schema);
+      const format = options.format ?? "json";
+      if (format === "json") {
+        console.log(jsonFormat);
+      } else {
+        console.log(ymalFormat);
+      }
+    } catch (err) {}
+  });
 
 program.parse(process.argv);
